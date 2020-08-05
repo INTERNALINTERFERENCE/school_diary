9a83e08bc0665c06865ccba3a9e4ecf93173dd37
Anton Ryabuhin <Lordd871@gmail.com>
2020-08-05 12:30:12 +1000

    first commit

---
+++ CollegeDiary/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeDiary.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Grade { get; set; }
+    }
+}
---
+++ CollegeDiary/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeDiary.Models
+{
+    public class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<string> TeacherTeachSubjects { get; set; }
+    }
+}
