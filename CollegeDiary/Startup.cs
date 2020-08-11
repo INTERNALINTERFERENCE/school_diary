547f042c4047f6d82080396d0846f254605d1161
Anton Ryabuhin <Lordd871@gmail.com>
2020-08-11 21:40:40 +1000

    add AccountController and some views; some changes in startup.cs

--- CollegeDiary/Startup.cs
+++ CollegeDiary/Startup.cs
@@ -16,6 +16,7 @@ namespace CollegeDiary
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,10 @@ namespace CollegeDiary
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{controller}/{action}",
+                    new { controller = "Account", action = "Login" });
             });
         }
     }
